da0d963f828e4a5d45b5bf69b4c9527660de97ed
Andrew Jo <andrew@verdigris.co>
2019-02-23 14:48:46 -0800

    Create RedisCache class

---
+++ src/Drivers/Cache/Redis/index.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const { ioc } = require('@adonisjs/fold')
+const Cache = require('..')
+
+class RedisCache extends Cache {
+  constructor() {
+    super()
+    this.Redis = ioc.use('Redis')
+  }
+
+  /**
+   * Generate cache.
+   * @param {String} key
+   * @param {Mixed} value
+   * @param {Integer} milliseconds
+   *
+   * @return {TimeoutPointer}
+   */
+  put(key, value, milliseconds) {
+    this.Redis.set(key, value, 'px', milliseconds)
+  }
+
+  /**
+   * Get stored data by key.
+   * @param {String} key
+   *
+   * @return {Mixed}
+   */
+  get(key) {
+    return this.Redis.get(key)
+  }
+
+  /**
+   * Increment stored value by one.
+   * @param {String} key
+   *
+   * @return {Cache}
+   */
+  increment(key) {
+    this.Redis.incr(key)
+    return this
+  }
+
+  /**
+   * Increment expiration of stored data by number of seconds.
+   * @param {String} key
+   * @param {Integer} seconds
+   *
+   * @return {Cache}
+   */
+  incrementExpiration(key, seconds) {
+    this.Redis.expire(key, seconds)
+    return this
+  }
+}
+
+module.exports = RedisCache
