c95b009b681bfd6e1966b3e64c8b388ba66e911b
Andrew Jo <andrew@verdigris.co>
2019-02-25 12:18:16 -0800

    Update author section

--- providers/ThrottleProvider.js
+++ providers/ThrottleProvider.js
@@ -3,7 +3,7 @@
 /**
  * adonis-throttle
  *
- * (c) Ron Masas <ronmasas@gmail.com>
+ * (c) Ron Masas <ronmasas@gmail.com>, Andrew Jo <andrewjo@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
